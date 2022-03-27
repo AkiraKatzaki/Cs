aea3241b3e487971599b96c8a7cb3654be526f8e
Konstantin <Akira.katzaki@gmail.com>
2022-03-27 13:59:07 +0500

    add task 6 in repos

---
+++ Seminar1/Task6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        Console.WriteLine($"Введите число: ");
+        var i = Console.ReadLine();
+        int a = Convert.ToInt32(i);
+
+        if (a % 2 == 0)
+            Console.WriteLine("Число четное");
+        else
+            Console.WriteLine("Число не четное");
+    }
+}
